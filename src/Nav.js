bd9a504fce71cd0d6f1275d19bf7d47149b4f0d2
Immanuel Mundary <e33a3u3l@gmail.com>
2022-06-12 19:39:55 +0530

    progress

--- src/Nav.js
+++ src/Nav.js
@@ -5,19 +5,24 @@ import './Nav.css'
 
 
 function Nav() {
-  const [show,handleShow]=useState(false);
+  const [alpla,handleAlpha] = useState("#11111100");
 
   useEffect(() => {
     window.addEventListener("scroll",() => {
-      if(window.scrollY > 100){
-        handleShow(true);
+      if(window.scrollY > 201){
+        handleAlpha("#111");
       }
-      else handleShow(false);
+      else if (window.scrollY==0){
+        handleAlpha("#11111100");
+      }
+      else {
+        handleAlpha("#111111"+Math.round(((window.scrollY/200)*255)).toString(16).toUpperCase());
+      }
     } );
   },[]);
 
   return (  
-    <div className={`topbar ${show && "nav_black"}`} >
+    <div className={`topbar`} style={{background:`${alpla}`}} >
         <img className='logoes' src={logo} alt="Fetnlix Logo" />
         <img className='profiles' src={propic} />
     </div>
