6a6c05ee34958133c168d272337f96e6d482f515
Immanuel Mundary <immanuelmundary09@google.com>
2023-04-06 00:58:18 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Nav/>
       <Banner/>
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} is_large/>
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
